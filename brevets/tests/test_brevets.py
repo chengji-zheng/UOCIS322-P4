14d8600a01d878c959919de0356984cf031d78e5
chengji-zheng <63076684+chengji-zheng@users.noreply.github.com>
2021-06-11 22:49:05 -0700

    Final Commit  (CZ)

--- brevets/tests/test_brevets.py
+++ brevets/tests/test_brevets.py
@@ -18,7 +18,7 @@ def test_open_times():
     """
     A series of testing that testing open_times in 0, 60, 200, 300, 400, 600, 1000km
     """
-    testing_time = arrow.now.format()
+    testing_time = arrow.get("2021-01-01T01:01", 'YYYY-MM-DDTHH:mm')
     assert str(open_time(0, 200, testing_time)) == str(testing_time)                        # Testing for 0km
     assert str(open_time(60, 200, testing_time)) == str(testing_time.shift(minutes=106))    # Testing for 60km
     assert str(open_time(200, 200, testing_time)) == str(testing_time.shift(minutes=353))   # Testing for 200km
@@ -31,7 +31,7 @@ def test_close_time():
     """
     A series of testing that testing close_times in 0, 60, 200, 300, 400, 600, 1000km
     """
-    testting_time = arrow.now.format()
+    testing_time = arrow.get("2021-01-01T01:01", 'YYYY-MM-DDTHH:mm')
     assert str(close_time(0, 200, testing_time)) == str(testing_time.shift(minutes=60))      # Testing for 0km
     assert str(close_time(60, 200, testing_time)) == str(testing_time.shift(minutes=240))    # Testing for 60km
     assert str(close_time(200, 200, testing_time)) == str(testing_time.shift(minutes=800))   # Testing for 200km
@@ -44,7 +44,7 @@ def test_invalid_user_input():
     """
     Testing different of invalid inputs
     """
-    testting_time = arrow.now.format()
+    ttesting_time = arrow.get("2021-01-01T01:01", 'YYYY-MM-DDTHH:mm')
     assert open_time(-60, 200, testing_time)                    # Testing with a negative number
     assert close_time(-60, 200, testing_time)
     assert open_time(60.123456, 200, testing_time)              # Testing with a float number
