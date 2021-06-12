888c3ab0081f531d918587a7427ee1032ebec97a
chengji-zheng <63076684+chengji-zheng@users.noreply.github.com>
2021-06-11 22:16:38 -0700

    Final Commit  (CZ)

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -30,6 +30,8 @@ def open_time(control_dist_km, dist, brevet_start_time):
     max_speeds = [34, 32, 30, 28, 28, 26]           # A list of max_speeds by different distances
     step = 200                                      # Chunk every max_speed by a step of 200km distaces
     time = 0                                        # Time stored in minutes
+    if control_dist_km < 0:
+        return "Error"
     if control_dist_km >= dist:                     # Handles the bad control points that set outside the brevets, it will return an error message on the client side
         control_dist_km = dist
     for i in range(len(max_speeds)):                
@@ -56,7 +58,11 @@ def close_time(control_dist_km, dist, brevet_start_time):
     min_speeds = [15, 15, 15, 11.428, 11.428, 13.333]
     step = 200                                                  # Chunk every min_speed by a step of 200km distaces
     time = 0                                                    # Still in minutes               
-
+    
+    if control_dist_km < 0:
+        return "Error"
+    if control_dist_km >= dist:                     # Handles the bad control points that set outside the brevets, it will return an error message on the client side
+        control_dist_km = dist
     for j in range(len(min_speeds)):
         if control_dist_km > step:
             time += round(((step / min_speeds[j]) * 60), 0)
@@ -64,7 +70,7 @@ def close_time(control_dist_km, dist, brevet_start_time):
         else:
             # Be careful the special rule that when control points located at less than 60 km.
             if control_dist_km < 60:
-                time += round((((control_dist_km // 20) + 1) * 60), 0)
+                time += round((((control_dist_km / 20) + 1) * 60), 0)
             else:
                 time += round(((control_dist_km / min_speeds[j]) * 60), 0)
             return  brevet_start_time.shift(minutes=time)
