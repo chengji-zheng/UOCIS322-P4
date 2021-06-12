a0afa0bcfff91113cc4cf7e213d57ff191b80169
chengji-zheng <63076684+chengji-zheng@users.noreply.github.com>
2021-06-11 23:14:35 -0700

    Final Commit  (CZ)

--- brevets/tests/test_brevets.py
+++ brevets/tests/test_brevets.py
@@ -22,10 +22,10 @@ def test_open_times():
     assert str(open_time(0, 200, testing_time)) == str(testing_time)                        # Testing for 0km
     assert str(open_time(60, 200, testing_time)) == str(testing_time.shift(minutes=106))    # Testing for 60km
     assert str(open_time(200, 200, testing_time)) == str(testing_time.shift(minutes=353))   # Testing for 200km
-    assert str(open_time(300, 200, testing_time)) == str(testing_time.shift(minutes=541))   # Testing for 400km
+    assert str(open_time(300, 300, testing_time)) == str(testing_time.shift(minutes=541))   # Testing for 400km
     assert str(open_time(400, 400, testing_time)) == str(testing_time.shift(minutes=728))   # Testing for 400km
     assert str(open_time(600, 600, testing_time)) == str(testing_time.shift(minutes=1128))   # Testing for 600km
-    assert str(open_time(1000,1000, testing_time)) == str(testing_time.shift(minutes=1985))   # Testing for 1000km
+    assert str(open_time(1000,1000, testing_time)) == str(testing_time.shift(minutes=1986))   # Testing for 1000km
 
 def test_close_time():
     """
@@ -35,7 +35,7 @@ def test_close_time():
     assert str(close_time(0, 200, testing_time)) == str(testing_time.shift(minutes=60))      # Testing for 0km
     assert str(close_time(60, 200, testing_time)) == str(testing_time.shift(minutes=240))    # Testing for 60km
     assert str(close_time(200, 200, testing_time)) == str(testing_time.shift(minutes=800))   # Testing for 200km
-    assert str(close_time(300, 200, testing_time)) == str(testing_time.shift(minutes=1200))   # Testing for 400km
+    assert str(close_time(300, 300, testing_time)) == str(testing_time.shift(minutes=1200))   # Testing for 400km
     assert str(close_time(400, 400, testing_time)) == str(testing_time.shift(minutes=1600))   # Testing for 400km
     assert str(close_time(600, 600, testing_time)) == str(testing_time.shift(minutes=2400))   # Testing for 600km
     assert str(close_time(1000,1000, testing_time)) == str(testing_time.shift(minutes=4500))   # Testing for 1000km
@@ -44,7 +44,7 @@ def test_invalid_user_input():
     """
     Testing different of invalid inputs
     """
-    ttesting_time = arrow.get("2021-01-01T01:01", 'YYYY-MM-DDTHH:mm')
+    testing_time = arrow.get("2021-01-01T01:01", 'YYYY-MM-DDTHH:mm')
     assert open_time(-60, 200, testing_time)                    # Testing with a negative number
     assert close_time(-60, 200, testing_time)
     assert open_time(60.123456, 200, testing_time)              # Testing with a float number
